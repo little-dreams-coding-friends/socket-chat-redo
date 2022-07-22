aaac8a09701b5965148d45108eac83e08917f090
ZhouSusan <szhou089@gmail.com>
2022-07-22 02:11:26 -0700

    created Connection class and setted up callbacks on events coming from the socket

---
+++ chat-server/chat.js
@@ -0,0 +1,14 @@
+class Connection {
+    constructor (io, socket) {
+        this.socket = socket;
+        this.io = io;
+
+        socket.on('getMessages', () => this.getMessages());
+        socket.on('message', (value) =>  this.handleMessage(value));
+        socket.on('disconnect', () => this.disconnet());
+        socket.on('connect_error', (err) => {
+            console.log('connect_error due to ' + err.message);
+        });
+    }
+}
+
