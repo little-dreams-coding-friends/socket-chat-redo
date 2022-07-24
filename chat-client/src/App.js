1eebfe9b1bac5382b49ebd4b5bd92621f243cce0
ZhouSusan <szhou089@gmail.com>
2022-07-23 19:51:35 -0700

    fixed misspelling on line 12

--- chat-client/src/App.js
+++ chat-client/src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io(`httlp://${window.location.hostname}: 3000`);
+    const newSocket = io(`http://${window.location.hostname}: 3000`);
     setSocket(newSocket);
 
     return () => newSocket.close();
