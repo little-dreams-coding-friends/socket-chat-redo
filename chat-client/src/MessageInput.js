3d62214e87554dbed0ee96af5c3a722404895c98
ZhouSusan <szhou089@gmail.com>
2022-07-23 19:30:31 -0700

    added Message Input component with event handler. This handler will emit message event on socket and pass along the field value

---
+++ chat-client/src/MessageInput.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+
+const NewMessage = ({socket}) => {
+    const [value, setValue] = useState('');
+
+    const submitFormHandler = (e) => {
+        e.preventDefault();
+        socket.emit('message', value);
+        setValue('');
+    }
+
+    return (
+        <form onSubmit={submitFormHandler}>
+            <input value={value} onChange={(e) => {
+                setValue(e.currentTarget.value);
+            }}/>
+        </form>
+    )
+}
+
+module.exports = NewMessage;
--- chat-client/src/Messages.js
+++ chat-client/src/Messages.js
@@ -1,4 +1,4 @@
-import Ract, {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 function Messages({socket}) {
     const [messages, setMessages] = useState({});
@@ -19,8 +19,27 @@ function Messages({socket}) {
                 return newMsg;
             })
         };
-    });
 
+        socket.on('message', messageListener);
+        socket.on('deleteMessage', deleteMessageListener);
+        socket.emit('getMessages');
+        
+        return () => {
+            socket.off('message', messageListener);
+            socket.off('deleteMessage', deleteMessageListener);
+        };
+    }, [socket]);
+
+    return (
+        <div>
+            {[...Object.values(messages)].sort((a, b) => a.time - b.time).map((message) => (
+                <div key={message.id} title={`Sent at ${new Date(message.time).toLocaleDateString()}`}>
+                    <p>{message.value}</p>
+                    <p>{new Date(message.time).toLocaleTimeString()}</p>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 module.exports = Messages;
